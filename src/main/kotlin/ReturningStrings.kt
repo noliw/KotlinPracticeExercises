ae486aa8aec3c056e525139653396fb170c6c46f
noliw <noliw2017@gmail.com>
2023-04-06 00:10:44 -0700

    Return Strings Exercise

---
+++ src/main/kotlin/ReturningStrings.kt
@@ -0,0 +1,5 @@
+/*
+Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
+ */
+
+fun greet(name: String): String = "Hello, $name how are you doing today?"
