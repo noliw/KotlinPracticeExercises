0fd9cecabdefa1ba1d5d3bdc3e60bd525bd78cce
noliw <noliw2017@gmail.com>
2023-04-06 06:06:34 -0700

    Return Strings Exercise

---
+++ src/main/kotlin/ArraysAndLists.kt
@@ -0,0 +1,10 @@
+fun main() {
+//    var favMovies = mutableListOf("Creed", "Black Panther", "Ye Fikir ABCD")
+//    favMovies.add("love island")
+//
+//    println(favMovies)
+    var nums = arrayOf(1, 2, 3, 20, 13, 8)
+    nums.sort()
+    println(nums.joinToString(", "))
+
+}
