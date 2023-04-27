5e166eccb3a650f38702193137d9fa352fc82f22
noliw <noliw2017@gmail.com>
2023-04-27 04:51:11 -0700

    functions final

--- src/main/kotlin/functions1.kt
+++ src/main/kotlin/functions1.kt
@@ -1,6 +1,29 @@
 fun main(args: Array<String>) {
-    val max = getMax(5,8)
+    val max = getMax(5, 8)
     println(max)
+    println("------------------------")
+    println()
+//    default params
+    sendMessage("Noli")
+
+    println("----------------------------")
+    println()
+//    named param
+    sendMessage(message = "Ollaaaaa")
+    println("----------------------------")
+    println()
+//    vararg keyword
+    println(sum(5,6,7,10, 56))
+
 }
 
-fun getMax(a: Int, b:Int) = if(a > b) a else b
+fun getMax(a: Int, b: Int) = if (a > b) a else b
+
+//Default Params
+
+fun sendMessage(name: String = "dad", message: String = "No message") {
+    println("$name has the following message: $message")
+}
+
+// vararg keyword
+fun sum(vararg numbers: Int) = numbers.sum()
