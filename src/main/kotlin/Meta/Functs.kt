b437d9bb5bd8bb3d161a75bb2a95b87881669295
noliw <noliw2017@gmail.com>
2023-05-18 05:07:59 -0700

    functions

---
+++ src/main/kotlin/Meta/Functs.kt
@@ -0,0 +1,19 @@
+package Meta
+
+//Add your function below this line
+fun getPoints(basePoints: Int, boost: Int) =  basePoints * boost
+
+
+fun main() {
+    var score = 0;
+    println(score)
+    score += getPoints(10, 1)
+    println(score)
+    score += getPoints(20, 2)
+    println(score)
+    score += getPoints(-10, 1)
+    println(score)
+    score += getPoints(5, 3)
+    println(score)
+    score += getPoints(-15, 2)
+}
