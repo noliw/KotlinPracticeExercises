99dba66e45f1b22c81e3260ff266985d12895e14
noliw <noliw2017@gmail.com>
2023-06-05 13:38:56 -0700

    Android 12 classes

--- src/main/kotlin/androidKotlin12/Lists.kt
+++ src/main/kotlin/androidKotlin12/Lists.kt
@@ -1,2 +1,18 @@
 package androidKotlin12
 
+/* ->Lists are ordered collections of elements, meaning the elements have a specific order or sequence.
+    -> Elements in a list can be accessed and retrieved using their index or position.
+    -> Lists allow duplicate elements, which means you can have multiple occurrences of the same element in a list.
+    -> Lists are represented by classes like ArrayList or LinkedList in Kotlin.
+    -> Lists are typically used when you need to maintain the order of elements or when duplicates are allowed.*/
+
+
+fun main() {
+    val months = listOf("Jan", "Oct", "Mar")
+
+//    println(months[2])
+    for (month in months){
+        println(month)
+    }
+
+}
